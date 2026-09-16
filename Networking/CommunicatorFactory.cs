f7ef066ead7b946de06cbbdf5f9d739a525d6a46
Bharath K <112301013@smail.iitpkd.ac.in>
2026-09-16 19:07:30 +0530

    Add project files.

---
+++ Networking/CommunicatorFactory.cs
@@ -0,0 +1,14 @@
+﻿using System.Diagnostics;
+
+namespace Networking
+{
+    public static class CommunicatorFactory
+    {
+        public static ICommunicator CreateCommunicator()
+        {
+            ICommunicator communicator = new UdpCommunicator();
+            Trace.TraceInformation($"Starting UDP Communicator in port {communicator.ListenPort}");
+            return communicator;
+        }
+    }
+}
---
+++ Networking/ICommunicator.cs
@@ -0,0 +1,16 @@
+﻿using System.Diagnostics;
+
+
+namespace Networking;
+
+public interface ICommunicator
+    {
+    int ListenPort { get; }
+
+    void AddSubscriber(string id, IMessageListener subscriber);
+
+    void RemoveSubscriber(string id);
+
+    void SendMessage(string IpAdress, int port, string senderId, string message);
+}
+
